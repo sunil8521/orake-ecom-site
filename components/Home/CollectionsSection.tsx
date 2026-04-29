9d0af27f4a1d85f18c3acba830b568bef08d1122
Aryan Kumar Singh <aryankumarsingh99@gmail.com>
2026-04-29 22:19:57 +0530

    fix: adjust top padding of CollectionsSection for improved layout

--- components/Home/CollectionsSection.tsx
+++ components/Home/CollectionsSection.tsx
@@ -96,7 +96,7 @@ export default function CollectionsSection() {
   };
 
   return (
-    <section className="bg-white pt-48 pb-24 px-12 sm:px-16 lg:px-28 overflow-hidden">
+    <section className="bg-white pt-19 pb-24 px-12 sm:px-16 lg:px-28 overflow-hidden">
       <div className="max-w-[1400px] mx-auto">
         {/* Gen Z Title Style */}
         <motion.div
